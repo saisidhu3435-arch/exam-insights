e04ae37e9b0e1607588c16502d320167e1f4a54c
Siddarth <saisidhu3435@gmail.com>
2026-05-04 14:06:22 +0530

    fixed news + auto refresh

--- api/news.ts
+++ api/news.ts
@@ -1,11 +1,15 @@
-import { cachedNews } from "./update-news"
-
 export default async function handler(req, res) {
   try {
+    const response = await fetch(
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=366aebe07bb74ff0943ebc3cc5638d4a"
+    )
+
+    const data = await response.json()
+
     res.status(200).json({
-      articles: cachedNews
+      articles: data.articles || []
     })
   } catch (err) {
-    res.status(500).json({ error: "Failed to get news" })
+    res.status(500).json({ error: "Failed to fetch news" })
   }
 }
